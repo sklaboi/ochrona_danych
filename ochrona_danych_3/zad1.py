e7f9a7cef2bcd37fd84fbbf084f67aa57554b277
aquarioos <dtaranta@gmail.com>
2015-03-24 01:41:01 +0100

    ochrona 3 z entropią

--- ochrona_danych_3/zad1.py
+++ ochrona_danych_3/zad1.py
@@ -1,6 +1,7 @@
 import sys
 from Crypto.Cipher import DES, AES
 from Crypto.Random import get_random_bytes
+import math
 
 def _normalize(text, req):
     i = 0
@@ -17,6 +18,22 @@ def _to_hex(text):
             tmp = '0' + tmp
         out += tmp
     return out
+def _entropy_hex(text):
+    out = 0
+    tab = [float(0)] * 300
+    cn = 0
+    for i in range(0, len(text), 2):
+        h = text[i: i + 2]
+        ir = int(h, 16)
+        tab[ir] += 1
+        cn += 1
+    for i in tab:
+        i /= cn
+        if i > 0:
+            out += i * math.log(i, 2)
+        else:
+            continue
+    return abs(out)
 
 def main():
     print 'enter input:'
@@ -34,18 +51,22 @@ def main():
     des = DES.new(key8, DES.MODE_ECB)
     enc = des.encrypt(inp)
     print '\tencoded:', _to_hex(enc)
+    print '\tentropy:', _entropy_hex(_to_hex(enc))
     print 'DES, CBC mode'
     des = DES.new(key8, DES.MODE_CBC, iv8)
     enc = des.encrypt(inp)
     print '\tencoded:', _to_hex(enc)
+    print '\tentropy:', _entropy_hex(_to_hex(enc))
     print 'AES, ECB mode'
     aes = AES.new(key16, AES.MODE_ECB)
     enc = aes.encrypt(inp)
     print '\tencoded:', _to_hex(enc)
+    print '\tentropy:', _entropy_hex(_to_hex(enc))
     print 'DES, CBC mode'
     aes = AES.new(key16, AES.MODE_CBC, iv16)
     enc = aes.encrypt(inp)
     print '\tencoded:', _to_hex(enc)
+    print '\tentropy:', _entropy_hex(_to_hex(enc))
 try:
     main()
 except ValueError as ve:
